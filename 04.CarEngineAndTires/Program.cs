5fe5e04ca1a49fcd4fa6cc734bfbe8ae12a76629
Teddy Aleksov <teddy57603@gmail.com>
2021-01-30 16:25:48 +0200

    updated

---
+++ 04.CarEngineAndTires/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tires = new Tire[4];
+
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+        }
+    }
+}
